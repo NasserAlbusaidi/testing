4cab71b33a056865bf7fb0eb5143dcadc9325495
Nasser Albusaidi <74050604+NasserAlbusaidi@users.noreply.github.com>
2024-05-14 15:43:49 +0400

    Remove AddToHomeScreen component from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import AddToHomeScreen from './AddToHome';
 import './App.css';
 import Quiz from './Quiz';
 
 function App() {
   return (
     <div className="App">
-      <AddToHomeScreen />
       <Quiz />
     </div>
   );
